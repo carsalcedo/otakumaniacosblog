de2a1116b4697122981e2202df0aa5420ae9b8c7
carsalcedo <fercarrera07@gmail.com>
2022-12-09 17:23:31 -0400

    menu por categorias

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,33 +1,23 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
 
 const Menu = ({categories}) => {
-  //quede en hora 2:00:00
-    const posts = [
-        {
-          id: 1,
-          title: "Publicacion 1",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis explicabo itaque eligendi neque vitae accusantium sit blanditiis accusamus quisquam, quia quae quibusdam nobis, vel atque rem beatae culpa praesentium sint.",
-          img: "https://i.postimg.cc/yxP89zpf/315518696-3377187019276117-6214524369378497924-n.jpg"
-        },
-        {
-          id: 2,
-          title: "Publicacion 2",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis explicabo itaque eligendi neque vitae accusantium sit blanditiis accusamus quisquam, quia quae quibusdam nobis, vel atque rem beatae culpa praesentium sint.",
-          img: "https://i.postimg.cc/8Pp5Gjsx/313842235-1097325127489292-7136727792422890862-n.jpg"
-        },
-        {
-          id: 3,
-          title: "Publicacion 3",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis explicabo itaque eligendi neque vitae accusantium sit blanditiis accusamus quisquam, quia quae quibusdam nobis, vel atque rem beatae culpa praesentium sint.",
-          img: "https://i.postimg.cc/MT1T6B7J/313974371-870023427744502-8839034623788369530-n.jpg"
-        },
-        {
-          id: 4,
-          title: "Publicacion 4",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis explicabo itaque eligendi neque vitae accusantium sit blanditiis accusamus quisquam, quia quae quibusdam nobis, vel atque rem beatae culpa praesentium sint.",
-          img: "https://i.postimg.cc/3N7wdw14/315402866-169446752366740-2243257093549586679-n.jpg"
-        },
-      ]
+  
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/?categories=${categories}`);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [categories]);
 
   return (
     <div className="menu">
@@ -36,7 +26,9 @@ const Menu = ({categories}) => {
             <div className="post" key={post.id}>
                 <img src={post.img} alt={post.title} />
                 <h2>{post.title}</h2>
-                <button>Leer mas</button>
+                <Link className='link' to={`/post/${post.id}`}>
+                 <button>Leer mas</button>
+                </Link>
             </div>
         ))}
     </div>
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -5,11 +5,20 @@ import 'react-quill/dist/quill.snow.css';
 const Write = () => {
 
   const [value, setValue] = useState('');
+  const [title, setTitle] = useState('');
+  const [file, setFile] = useState(null);
+  const [categories, setCategories] = useState('');
+
+  const handleClick = async e => {
+    e.preventDefault()
+    //quede en hora 2:03:26
+  }
+
   console.log(value);
   return (
     <div className='add'>
       <div className="content">
-        <input type="text" name="" placeholder='titulo' />
+        <input type="text" name="" placeholder='titulo' onChange={e => setTitle(e.target.value)} />
         <div className="editorContainer">
            <ReactQuill className='editor' theme="snow" value={value} onChange={setValue} />;
         </div>
@@ -23,37 +32,37 @@ const Write = () => {
           <span>
             <b>Visibilidad:</b> Publico
           </span>
-          <input style={{display:"none"}} type="file" name="" id="file" />
+          <input style={{display:"none"}} type="file" name="" id="file" onChange={e => setFile(e.target.files[0])}  />
           <label className='file' htmlFor="file">Cargar imagen</label>
           <div className="buttons">
             <button>Guardar como Reclutar</button>
-            <button>Actualizar</button>
+            <button onClick={handleClick}>Publicar</button>
           </div>
         </div>
         <div className="item">
           <h1>Categoria</h1>
           <div className="cat">
-            <input type="radio" name="categories" id="art" value="art" />
+            <input type="radio" name="categories" id="art" value="art" onChange={e => setCategories(e.target.value)} />
             <label htmlFor="art">Arte</label>
           </div>
           <div className="cat">
-            <input type="radio" name="categories" id="evento" value="evento" />
+            <input type="radio" name="categories" id="evento" value="evento" onChange={e => setCategories(e.target.value)} />
             <label htmlFor="evento">Eventos</label>
           </div>
           <div className="cat">
-            <input type="radio" name="categories" id="show" value="show" />
+            <input type="radio" name="categories" id="show" value="show" onChange={e => setCategories(e.target.value)} />
             <label htmlFor="show">Shows</label>
           </div>
           <div className="cat">
-            <input type="radio" name="categories" id="concurso" value="concurso" />
+            <input type="radio" name="categories" id="concurso" value="concurso" onChange={e => setCategories(e.target.value)} />
             <label htmlFor="concurso">Concursos</label>
           </div>
           <div className="cat">
-            <input type="radio" name="categories" id="kpop" value="kpop" />
+            <input type="radio" name="categories" id="kpop" value="kpop" onChange={e => setCategories(e.target.value)} />
             <label htmlFor="kpop">Kpop</label>
           </div>
           <div className="cat">
-            <input type="radio" name="categories" id="gamer" value="gamer" />
+            <input type="radio" name="categories" id="gamer" value="gamer" onChange={e => setCategories(e.target.value)} />
             <label htmlFor="gamer">Gamers</label>
           </div>
         </div>
